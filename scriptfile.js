8afe31011a6f805b3d142c4b846610ae603bb9ab
Nande03 <nandetoyi@yahoo.com>
2023-04-12 12:40:45 +0200

    js file

---
+++ scriptfile.js
@@ -0,0 +1,10 @@
+var firstName = document.getElementById("fname");
+var lastname = document.getElementById("sname");
+var occupation = document.getElementById("occupation");
+
+function submitFunc()
+{
+    console.log(firstName.value);
+    console.log(lastname.value);
+    console.log(occupation.value);
+}
